e17e7015d1800bcbf47b522a77e7092759db282a
dgamboa-baubap <didier@baubap.com>
2023-08-25 08:14:01 -0600

    Add baseline and colors
    score baseline and color at markers were added

--- app.py
+++ app.py
@@ -8,11 +8,13 @@ def load_data(file_name: str):
     return pd.read_csv(file_name, encoding='utf-8')
 
 # Function to plot data
-def plot_data(data, score, std, title):
+def plot_data(data, score, std, title, goal_score):
     scatter = go.Scatter(
         x=data['team'],
         y=data[score],
         mode='markers',
+        marker=dict(
+            color='#580BE6'),
         error_y=dict(
             type='data',
             array=data[std],
@@ -26,6 +28,13 @@ def plot_data(data, score, std, title):
     )
 
     fig = go.Figure(data = [scatter], layout=layout)
+    fig.add_hline(y=goal_score, line_dash="dot",
+                  annotation_text="goal score",
+                  annotation_position="top right",
+                  annotation_font_size=20,
+                  line_color="plum",
+                  annotation_font_color="plum"
+                  )
     st.plotly_chart(fig, use_container_width=True)
 
 
@@ -37,8 +46,8 @@ data = load_data('./data/data.csv')
 
 # Neural Network Model
 st.subheader("Neural Network Model")
-plot_data(data, "score", "std", "Neural Network Model")
+plot_data(data, "score", "std", "Neural Network Model", 0.99)
 
 # Time Series Model
 st.subheader("Time Series Model")
-plot_data(data, "score", "std", "Time Serie Model")
+plot_data(data, "score", "std", "Time Serie Model", 0.99)
