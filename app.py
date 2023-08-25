d01030146961ae25a89d9cf600044b595fdf489e
dgamboa-baubap <didier@baubap.com>
2023-08-25 07:58:22 -0600

    Update charts
    Chart for neuralnet and time series model score were added

--- app.py
+++ app.py
@@ -1,34 +1,44 @@
 import pandas as pd
 import streamlit as st
-import plotly.express as px
 import plotly.graph_objects as go
 
-
-st.set_page_config(page_title= 'Baubap AI Challenge - Scores')
 
 # Function to load data
 def load_data(file_name: str):
     return pd.read_csv(file_name, encoding='utf-8')
 
+# Function to plot data
+def plot_data(data, score, std, title):
+    scatter = go.Scatter(
+        x=data['team'],
+        y=data[score],
+        mode='markers',
+        error_y=dict(
+            type='data',
+            array=data[std],
+            visible=True
+        ))
+
+    layout = go.Layout(
+        yaxis_title="score",
+        xaxis_title="Teams",
+        yaxis_range=[0,1.2]
+    )
+
+    fig = go.Figure(data = [scatter], layout=layout)
+    st.plotly_chart(fig, use_container_width=True)
+
+
+# General Setup
+st.title('Baubap AI Challenge - Dashboard')
+
+# Load data
 data = load_data('./data/data.csv')
 
-scatter = go.Scatter(
-    x=data['team'],
-    y=data['score'],
-    mode='markers',
-    error_y=dict(
-        type='data',
-        array=data['std'],
-        visible=True
-    ))
+# Neural Network Model
+st.subheader("Neural Network Model")
+plot_data(data, "score", "std", "Neural Network Model")
 
-layout = go.Layout(
-    title="Score Table",
-    yaxis_title="Score",
-    xaxis_title="Teams",
-    yaxis_range=[0,1.2]
-)
-
-fig = go.Figure(data = [scatter], layout=layout)
-
-st.plotly_chart(fig, use_container_width=True)
+# Time Series Model
+st.subheader("Time Series Model")
+plot_data(data, "score", "std", "Time Serie Model")
