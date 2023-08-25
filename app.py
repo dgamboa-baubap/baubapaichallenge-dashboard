69d067c91cae3ec602d66226f56477fc2e84a0ed
dgamboa-baubap <didier@baubap.com>
2023-08-25 12:14:55 -0600

    changes on chart design
    Major changes to the charts including: size on marks, color on marks, title, show grid, rotation on y axis labels.

--- app.py
+++ app.py
@@ -8,12 +8,14 @@ def load_data(file_name: str):
     return pd.read_csv(file_name, encoding='utf-8')
 
 # Function to plot data
-def plot_data(data, score, std, title, goal_score):
+def plot_data(data, score, score_interval, std, title, threshold_score):
     scatter = go.Scatter(
         x=data['team'],
         y=data[score],
         mode='markers',
+        marker_symbol='diamond',
         marker=dict(
+            size = 12,
             color='#580BE6'),
         error_y=dict(
             type='data',
@@ -22,14 +24,16 @@ def plot_data(data, score, std, title, goal_score):
         ))
 
     layout = go.Layout(
-        yaxis_title="score",
+        yaxis_title="Score",
         xaxis_title="Teams",
-        yaxis_range=[0,1.2]
+        xaxis = dict(showgrid=True, tickangle = -90),
+        yaxis = dict(showgrid=True),
+        yaxis_range=score_interval
     )
 
     fig = go.Figure(data = [scatter], layout=layout)
-    fig.add_hline(y=goal_score, line_dash="dot",
-                  annotation_text="goal score",
+    fig.add_hline(y=threshold_score, line_dash="dot",
+                  annotation_text="threshold score: {}".format(threshold_score),
                   annotation_position="bottom right",
                   annotation_font_size=20,
                   line_color="plum",
@@ -42,12 +46,15 @@ def plot_data(data, score, std, title, goal_score):
 st.title('Baubap AI Challenge - Dashboard')
 
 # Load data
-data = load_data('./data/data.csv')
 
 # Neural Network Model
-st.subheader("Neural Network Model")
-plot_data(data, "score", "std", "Neural Network Model", 0.25)
+st.header("Neural Network Model")
+plot_data(load_data('./data/data_challenge01.csv'),
+          "score", [0,1], "std",
+          "Neural Network Model", 0.25)
 
 # Time Series Model
-st.subheader("Time Series Model")
-plot_data(data, "score", "std", "Time Serie Model", 0.99)
+st.header("Time Series Model")
+plot_data(load_data('./data/data_challenge02.csv'),
+          "score", [0.5e6,7.0e6], "std",
+          "Time Series Model", 1.5e6)
